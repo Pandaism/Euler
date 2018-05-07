5a30f381b4dc78536afa7dea389ef11222d502e8
Mike Nguyen <mike94.nguyen@yahoo.com>
2018-05-06 19:08:08 -0500

    Question 7

---
+++ Question_7/src/com/pandaism/euler/Main.java
@@ -0,0 +1,32 @@
+package com.pandaism.euler;
+
+import java.util.ArrayList;
+
+/**
+ * @author Pandaism
+ * @date 5/6/2018 : 6:53 PM
+ */
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Double> list = new ArrayList<>();
+        list.add(2D);
+
+        for(double i = 3; i < Double.MAX_VALUE; i += 2) {
+            if(isPrime(i)) list.add(i);
+
+            if(list.size() == 10001) break;
+        }
+
+        System.out.println(list);
+    }
+
+    private static boolean isPrime(double num) {
+        if(num % 2 == 0) return false;
+
+        for(double i = 3; i * i <= num; i += 2) {
+            if(num % i == 0) return false;
+        }
+
+        return true;
+    }
+}
