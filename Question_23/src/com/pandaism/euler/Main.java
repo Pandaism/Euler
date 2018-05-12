7eefda26e174bde19d6b1fe8ea6b037385aaa6d7
Mike Nguyen <mike94.nguyen@yahoo.com>
2018-05-12 16:55:18 -0500

    Question 23

---
+++ Question_23/src/com/pandaism/euler/Main.java
@@ -0,0 +1,76 @@
+package com.pandaism.euler;
+
+import java.util.ArrayList;
+
+/**
+ * @author Pandaism
+ * @date 5/12/2018 : 11:20 AM
+ *
+ * Refactor for more efficient algorithm
+ * @date 5/12/2018 : 4:54
+ */
+public class Main {
+    private static ArrayList<Integer> abundantNums = new ArrayList<>();
+    private static ArrayList<Integer> sumsOfAbundantNums = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        long start = System.currentTimeMillis();
+        for(int i = 1; i <= 28123; i++) {
+            if(isAbundant(i)) {
+                abundantNums.add(i);
+            }
+        }
+
+        for(int i = 0; i < abundantNums.size(); i++) {
+            int a = abundantNums.get(i);
+            for(int j = i; j < abundantNums.size() - i; j++) {
+                int b = abundantNums.get(j);
+                int sum = a + b;
+                if(!sumsOfAbundantNums.contains(sum)) {
+                    sumsOfAbundantNums.add(sum);
+                }
+            }
+        }
+
+        int sum = 0;
+
+        for(int i = 1; i <= 28123; i++) {
+            if(!sumsOfAbundantNums.contains(i)) {
+                sum += i;
+            }
+        }
+
+        System.out.println(sum);
+        System.out.println("\n" + (System.currentTimeMillis() - start));
+    }
+
+    private static boolean isAbundant(int num) {
+        return getDivisorsSum(num) > num;
+    }
+
+    private static int getDivisorsSum(int num) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+
+        int sum = 1;
+        int temp = num;
+
+        for(int i = 2; i <= temp; i++) {
+            if(num % i == 0) {
+                int result = num / i;
+                if(!list.contains(i) && !list.contains(result) && i != num && result != num) {
+                    list.add(i);
+                    sum += i;
+                    if(result != i) {
+                        list.add(result);
+                        sum += result;
+                    }
+                }
+                temp = result;
+            }
+        }
+
+        return sum;
+    }
+}
